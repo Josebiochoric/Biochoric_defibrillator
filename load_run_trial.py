36861b6e4f2e916e0e2fef068f4ff3e3ee79e510
Villarejo93 <josevilla93@gmail.com>
2024-07-11 22:02:59 +0200

    update 11 july 2024 _2

--- load_run_trial.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import sys
-
-# Specify the serial port and the file you want to execute
-serial_port = "/dev/tty.usbmodem14201"  # Replace with your serial port
-file_to_run = "path/to/your_script.py"  # Replace with your script's path
-
-# Run the pyboard.py script with subprocess
-subprocess.run([sys.executable, "path/to/pyboard.py", "--device", serial_port, "-f", file_to_run])
